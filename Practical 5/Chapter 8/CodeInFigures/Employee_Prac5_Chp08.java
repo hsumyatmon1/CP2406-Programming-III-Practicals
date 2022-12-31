e11f32b46e9f602093724d232a8dc188b87bb55d
hsumyatmon <hsumyatmon278@gmail.com>
2022-12-31 19:08:16 +0800

    Finish Practical 5

---
+++ Practical 5/Chapter 8/CodeInFigures/Employee_Prac5_Chp08.java
@@ -0,0 +1,18 @@
+public class Employee_Prac5_Chp08
+{
+   private int empNum;
+   private double empSal;
+   Employee_Prac5_Chp08(int e, double s)
+   {
+      empNum = e;
+      empSal = s;
+   }
+   public int getEmpNum()
+   {
+      return empNum;
+   }
+   public double getSalary()
+   {
+      return empSal;
+   }
+}
